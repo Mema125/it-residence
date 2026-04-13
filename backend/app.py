c403e75096dd3c8f9f435c9d338e56373feabba6
GohTia <gohtiamema33@gmail.com>
2026-04-13 10:28:38 +0000

    chore: initialisation

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Bienvenue sur mon API"})
+
+@app.route('/users', methods=['GET'])
+def users():
+    return jsonify([
+        {"id": 1, "nom": "Jean"},
+        {"id": 2, "nom": "Marie"}
+    ])
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    return jsonify({"message": "Utilisateur créé", "data": data}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
